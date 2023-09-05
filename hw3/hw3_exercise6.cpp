839f63448d4484e8437a7419f6369c5ed5b2bcaa
Ira Narang <iranarang@nkn-mbp2.lan>
2023-09-04 21:54:03 -0500

    hw3

--- hw3/hw3_exercise6.cpp
+++ hw3/hw3_exercise6.cpp
@@ -2,43 +2,45 @@
 // EID: in2933
 // TACC username: iranarang
 
-#include <iostream>
+#include <iostream> //add iostream, vector, and std libraries
 #include <vector>
 using namespace std;
 
-int main() {
+void exercise6(vector<int> numbers) { //create function to accept input of numbers
 
-    //vector<int> numbers = {1, 2, 3, 4, 5};
-    vector<int> numbers;
-    numbers.push_back(1);
-    numbers.push_back(2);
-    numbers.push_back(3);
-    numbers.push_back(4);
-    numbers.push_back(5);
-    vector<int> odds;
+    vector<int> odds; //separate the numbers into odds and events
     vector<int> evens;
     int length = numbers.size();
 
-    for (int i=0; i < length; i++) {
+    for (int i=0; i < length; i++) { //go through each number in the input
 
-        if (numbers[i] % 2 == 0) {
+        if (numbers[i] % 2 == 0) { //if number is even add to even vector
             evens.push_back(numbers[i]);
         }
-        else if (numbers[i] % 2 == 1) {
+        else if (numbers[i] % 2 == 1) { //if number is odd add to odd vector
             odds.push_back(numbers[i]);
         }
     }
 
-    cout << "Even vector: ";
+    cout << "Even vector: "; //print out even vector
     for (int i=0; i<evens.size(); i++) {
 
         cout << evens[i] << " ";
     }
     cout << endl;
-    cout << "Odd vector: ";
+    cout << "Odd vector: "; //print out odd vector
     for (int i=0; i<odds.size(); i++) {
 
         cout << odds[i] << " ";
     }
+
+}
+
+
+int main() {
+
+    vector<int> numbers = {1, 2, 3, 4, 5}; //add input
+
+    exercise6(numbers); //call the function
 
 }

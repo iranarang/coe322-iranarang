abf13ae8c2c90cdb5ba25ff9120afa05f7a9e9f2
Ira Narang <iranarang@nkn-mbp2.lan>
2023-12-05 20:06:42 -0600

    project

---
+++ project/main.h
@@ -0,0 +1,8 @@
+#pragma once
+void test_one_more_day();
+
+void one_more_day_with_varying_prob(int population_size, double contagion_probability);
+
+void simulation_for_few_days();
+
+void test_one_more_day_with_contacts();
---
+++ project/main2.h
@@ -0,0 +1,2 @@
+#pragma once
+void mutation_simulation();
